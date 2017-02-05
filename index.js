bea3349e88e9c7cc78b84a7d54bdc03271b9aeb6
Gautam Roy <zademathews@gmail.com>
2017-02-05 09:00:52 +0530

    Uploaded initial code
    Initial code for testing and UI planning.

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var Search = require('./lib/search.js');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/', function(req, res){
+
+  Search(req.body)
+  .then(function(data){
+      res.send(data);
+    }).catch(function(err){
+      console.error(err);
+    })
+});
+
+
+app.listen(8080, function(){
+  console.log("Server started on port 8080");
+})
