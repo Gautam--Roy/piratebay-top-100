577f50e4fbe9061eadb98117cdcb8c760974746d
Gautam Roy <zademathews@gmail.com>
2017-02-05 22:03:26 +0530

    Minor Updates to push to Heroku
    Minor Updates to push to Heroku

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var Search = require('./lib/search.js');
 var bodyParser = require('body-parser');
 
+app.set('port', (process.env.PORT || 8080));
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -29,6 +31,6 @@ app.get('/', function(req, res){
 });
 
 
-app.listen(8080, function(){
-  console.log("Server started on port 8080");
+app.listen(app.get('port'), function(){
+  console.log("Server started on port : ", app.get('port'));
 })
