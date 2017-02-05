95e4477cfb5f4159e94c20d1966f891915e5ca2a
Gautam Roy <zademathews@gmail.com>
2017-02-05 09:15:40 +0530

    fetch get request to root url
    fetch get request to root url

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.post('/', function(req, res){
     })
 });
 
+app.get('/', function(req, res){
+  res.send("Send data using post request");
+});
+
 
 app.listen(8080, function(){
   console.log("Server started on port 8080");
